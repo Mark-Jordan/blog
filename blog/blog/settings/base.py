5fc63e6f299dd44433368fed7f356beae2dfc782
fuchenzhilian <1743451479@qq.com>
2019-07-27 09:50:01 +0800

    model complete

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+    'config',
+    'comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
